14e09c8b94b84d984594238f7352460ef1855958
fel1pe-r-s <1felipersilva1@gmail.com>
2024-03-04 16:35:12 -0300

    validate check In

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -33,13 +33,30 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
     }
     return checkInOnSameDate;
   }
+  async findById(id: string) {
+    const checkIn = this.items.find((item) => item.id === id);
 
+    if (!checkIn) {
+      return null;
+    }
+
+    return checkIn;
+  }
   async findManyByUserId(userId: string, page: number) {
     return this.items
       .filter((checkIn) => checkIn.user_id === userId)
       .slice((page - 1) * 20, page * 20);
   }
 
+  async save(checkIn: CheckIn) {
+    const checkInIndex = this.items.findIndex((item) => item.id === checkIn.id);
+
+    if (checkInIndex >= 0) {
+      this.items[checkInIndex] = checkIn;
+    }
+
+    return checkIn;
+  }
   async countByUserId(userId: string): Promise<number> {
     return this.items.filter((checkIn) => checkIn.user_id === userId).length;
   }
